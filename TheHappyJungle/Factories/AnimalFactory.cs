672c6669f5770005f3d25c3fbe42b86ba28e8060
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2023-03-07 18:22:06 +0200

    Commit with adding logic for instantiation in AnimalFactory Class.

--- TheHappyJungle/Factories/AnimalFactory.cs
+++ TheHappyJungle/Factories/AnimalFactory.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using TheHappyJungle.Factories.Interfaces;
+using TheHappyJungle.Models.Animals;
 using TheHappyJungle.Models.Foods;
 using TheHappyJungle.Models.Interfaces;
 
@@ -11,7 +12,33 @@ namespace TheHappyJungle.Factories
     {
         public IAnimal CreateAnimal(string[] animalTokens)
         {
-            throw new NotImplementedException();
+            string type = animalTokens[0];
+            string name = animalTokens[1];
+            double weight = double.Parse(animalTokens[2]);
+
+            switch (type)
+            {
+                case "Hen":
+                    return new Hen(name,weight,double.Parse(animalTokens[3]));
+
+                case "Owl":
+                    return new Owl(name, weight, double.Parse(animalTokens[3]));
+
+                case "Mouse":
+                    return new Mouse(name, weight, animalTokens[3]);
+
+                case "Cat":
+                    return new Cat(name, weight, animalTokens[3], animalTokens[4]);
+
+                case "Dog":
+                    return new Dog(name, weight, animalTokens[3]);
+
+                case "Tiger":
+                    return new Cat(name, weight, animalTokens[3], animalTokens[4]);
+
+                default:
+                    throw new ArgumentException("Invalid type of animal");
+            }
         }
 
         public IFood CreateFood(string type, int quantity)
