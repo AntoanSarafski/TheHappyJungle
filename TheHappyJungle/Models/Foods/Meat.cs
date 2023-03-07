c816dc8a4ae5174eb9c63b7b915086d14fa8d5a9
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2023-03-07 16:30:28 +0200

    Commit with added classes for different type of Foods.

---
+++ TheHappyJungle/Models/Foods/Meat.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheHappyJungle.Models.Foods
+{
+    public class Meat : Food
+    {
+        public Meat(int quantity) 
+            : base(quantity)
+        {
+        }
+    }
+}
